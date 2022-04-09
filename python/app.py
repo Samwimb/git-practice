d0fd8387f9e8e462de39d13937d60485c354b141
Felipe Rivera <felipe@felipes-mini.lan>
2022-04-09 10:23:16 -0700

    setup for flask

--- python/app.py
+++ python/app.py
@@ -4,8 +4,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #! TODO: implement home page
+    #! TODO: implement home page SAM
+    s = validation(s)
     return "hello world"
+
+def validation(s):
+    """validate input
+
+    Args:
+        s (str): the input str
+    """
+    # ! TODO implement validation FELIPE
+    return s
 
 if __name__ == "__main__":
     app.run()
